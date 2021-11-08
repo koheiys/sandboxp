a27e2c8168f77ecc4f3daa034cdd0ce5c8692072
koheiys <46830064+koheiys@users.noreply.github.com>
2021-11-09 05:24:30 +0900

    Update DemoApplicationForSQ.java

--- demo/src/main/java/com/example/demo/DemoApplicationForSQ.java
+++ demo/src/main/java/com/example/demo/DemoApplicationForSQ.java
@@ -1,14 +1,6 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DemoApplicationForSQ {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplicationForSQ.class, args);
-	}
 
 	public void doSomething() {
 	}
