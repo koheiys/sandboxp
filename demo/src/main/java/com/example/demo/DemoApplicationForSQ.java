938dd36133b7d614e5ef03996ce91417215de525
koheiys <46830064+koheiys@users.noreply.github.com>
2021-11-09 04:55:56 +0900

    fix sq

--- demo/src/main/java/com/example/demo/DemoApplicationForSQ.java
+++ demo/src/main/java/com/example/demo/DemoApplicationForSQ.java
@@ -3,9 +3,14 @@ package com.example.demo;
 public class DemoApplicationForSQ {
 
 	public void doSomething() {
+		// do nothing.
 	}
 
 	public void doSomething2() {
+		// do nothing.
+	}
+
+	public void doSomething3() {
 	}
 
 	public int doSomething3() {
