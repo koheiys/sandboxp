7281d2a1b24b0aa5982a8ddea2fc70b89d21f65d
yasui.k <pollya20e@gmail.com>
2021-11-10 20:14:25 +0900

    up

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.demo.Demo2Utils;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,4 +13,9 @@ public class DemoApplication {
 
 	public void doSomething() {
 	}
+
+	public void deps() {
+		Demo2Utils.hello("test");
+	}
+
 }
---
+++ demo2/src/main/java/com/example/demo/Demo2Utils.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Demo2Utils {
+
+	public static String hello(String msg) {
+		return "hello " + msg;
+	}
+
+}
